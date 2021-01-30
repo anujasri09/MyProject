bd63ca66ee676c471a511314ab7a8fce562286af
Anuja <anuja.sri09@gmail.com>
2021-01-30 14:29:23 +0530

    next commit for learning

---
+++ f3.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "0\n",
+      "6\n",
+      "8\n",
+      "12\n",
+      "21\n",
+      "29\n",
+      "37\n",
+      "39\n",
+      "40\n"
+     ]
+    }
+   ],
+   "source": [
+    "#wap to print index of all occurences of 'p' \n",
+    "myStr=\"peter piper picked a peck of pickled peppers\"\n",
+    "count=0\n",
+    "ind=0\n",
+    "while ind<len(myStr):\n",
+    "    if(myStr[ind] == 'p'):\n",
+    "        print(ind)\n",
+    "    ind+=1\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
